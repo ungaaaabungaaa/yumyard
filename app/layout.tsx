46814949a8888f35875ec50b02686048da1fa34e
syed abdul muqeeth <samx123786@gmail.com>
2025-10-07 00:34:57 +0530

    Refactor globals.css to remove dark mode variables; update layout to set default theme to light and disable system theme; clean up Home component by removing dark mode specific classes.

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,8 +26,8 @@ export default function RootLayout({
       <body className={`${poppins.variable} antialiased`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          defaultTheme="light"
+          enableSystem={false}
           disableTransitionOnChange
         >
           <ConvexClientProvider>{children}</ConvexClientProvider>
