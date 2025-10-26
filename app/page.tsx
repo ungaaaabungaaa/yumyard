01b2e48e4ce54739fd8916c2d5553006c840c72f
syed abdul muqeeth <samx123786@gmail.com>
2025-10-26 19:23:01 +0530

    Update Home page to include Lottie animation and new layout for "Coming Soon" message. Enhance user experience with a visually appealing design featuring a centered animation and updated text styling.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
 "use client";
+import Lottie from "lottie-react";
+import ComingSoon from "@/public/lottie/Cooking egg.json";
+
 export default function Home() {
   return (
-    <>
-    <h1>Home Page</h1>
-    </>
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <div className="flex flex-col items-center justify-center space-y-8">
+        <div className="w-[45vh] h-[45vh]">
+          <Lottie animationData={ComingSoon} className="w-full h-full" loop={true} />
+        </div>
+        <h1 className="text-6xl font-black text-gray-800">Coming Soon!</h1>
+        <p className="text-2xl font-black ">YumYard<span className="text-typography-primary">.Cafe</span></p>
+      </div>
+    </div>
   );
 }
