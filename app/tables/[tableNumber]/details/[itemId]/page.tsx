12a2c35889359ea6247182693ec3eea8db1deaa8
syed abdul muqeeth <samx123786@gmail.com>
2025-11-06 00:10:17 +0530

    Refactor import paths in MenuItemDetails component for consistency
    - Updated import statements in the MenuItemDetails component to use absolute paths instead of relative paths, improving clarity and maintainability.

--- app/tables/[tableNumber]/details/[itemId]/page.tsx
+++ app/tables/[tableNumber]/details/[itemId]/page.tsx
@@ -2,8 +2,8 @@
 
 import { useParams } from 'next/navigation';
 import { useQuery } from 'convex/react';
-import { api } from '../../../../../../convex/_generated/api';
-import { Id } from '../../../../../../convex/_generated/dataModel';
+import { api } from '@/convex/_generated/api';
+import { Id } from '@/convex/_generated/dataModel';
 import { Spinner } from '@/components/ui/spinner';
 
 export default function MenuItemDetails() {
